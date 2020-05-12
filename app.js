6939e15fd2b0212dcd52e0edb5224a641601578e
JadeTyrer <jadetyrer@hotmail.co.uk>
2020-05-12 15:41:19 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const port = process.env.port || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Yo yo yo") 
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
